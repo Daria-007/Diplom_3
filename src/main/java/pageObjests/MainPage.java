677d77f2b21693af640d16145f55cc1bce905757
Daria Ermolaeva <ermolaeva.daria007@yandex.ru>
2024-04-28 14:59:33 +0300

    add third test

--- src/main/java/pageObjests/MainPage.java
+++ src/main/java/pageObjests/MainPage.java
@@ -1,11 +1,10 @@
 package pageObjests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Instant;
 
 
 public class MainPage {
@@ -16,9 +15,12 @@ public class MainPage {
     public static final String BUNS = ".//span[@class = 'text text_type_main-default' and text() = 'Булки']";
     public static final String CAUSE = ".//span[@class = 'text text_type_main-default' and text() = 'Соусы']";
     public static final String FILLINGS = ".//span[@class = 'text text_type_main-default' and text() = 'Начинки']";
-    public static final String UNLOGIN_BUTTON = ".//button[@class = 'Account_button__14Yp3 text text_type_main-medium text_color_inactive']";
+    public static final String UNLOGIN_BUTTON = ".//button[@class = 'Account_button__14Yp3 text text_type_main-medium text_color_inactive' and text() = 'Выход']";
     public static final String BUILD_BURGER_TEXT = ".//h1[@class = 'text text_type_main-large mb-5 mt-10']";
     public static final String LOGIN_BUTTON = ".//button[@class = 'button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_large__G21Vg']";
+    public static final String BUNS_TEXT = ".//h2[@class = 'text text_type_main-medium mb-6 mt-10' and text() = 'Булки']";
+    public static final String SAUSE_TEXT = ".//h2[@class = 'text text_type_main-medium mb-6 mt-10' and text() = 'Соусы']";
+    public static final String FILLINGS_TEXT = ".//h2[@class = 'text text_type_main-medium mb-6 mt-10' and text() = 'Начинки']";
     private WebDriver driver;
 
     public MainPage(WebDriver driver) {
@@ -48,8 +50,20 @@ public class MainPage {
     public void clickFilling() {
         driver.findElement(By.xpath(FILLINGS)).click();
     }
-    public void unLogin() {
+    public void unLogin()  {
         driver.findElement(By.xpath(UNLOGIN_BUTTON)).click();
+    }
+    public boolean bunsTextIsDisplayed() {
+
+        return driver.findElement(By.xpath(BUNS_TEXT)).isDisplayed();
+    }
+    public boolean sauseTextIsDisplayed() {
+
+        return driver.findElement(By.xpath(SAUSE_TEXT)).isDisplayed();
+    }
+    public boolean fillingsTextIsDisplayed() {
+
+        return driver.findElement(By.xpath(FILLINGS_TEXT)).isDisplayed();
     }
     public boolean BuilsBurgerDispayed() {
         WebDriverWait wait = new WebDriverWait(driver, 10);
